d60a7d8c77aba1c127b355869a3e66d3a49544ba
JuanpaTC <jp.tapia@uc.cl>
2024-10-13 21:44:56 -0300

    Actualizacion clean data

--- clean_data.py
+++ clean_data.py
@@ -7,72 +7,42 @@ import pandas as pd
 
 ########################### DATA_ITEMS ###########################
 
-'''
-Cosas por arreglar:
-    (1) Datos NA 
-    (2) Atributos en plural y singular
-    (3) Formato de atributos
-'''
-
 data_items = pd.read_csv('data_items.csv', delimiter=',')
-
+'''
+print(data_items[data_items['item_id']==1790])
 descartados = data_items[(data_items['size_m3'] == 0) | (data_items['size_m3'].isna()) | 
                          (data_items['storage_cost (CLP)'].isna()) | 
                          (data_items['cost_per_purchase'].isna())]['item_id'].tolist()
 items = data_items[~data_items['item_id'].isin(descartados)].copy()
-#print(descartados)
-#print(data_items[data_items['item_id'].isin(descartados)].copy()) ##### REVISAR
+'''
 
+items = data_items.copy()
 items.loc[:, 'group_description'] = items['group_description'].replace({'medicamentos': 'medicamento', 'accesorios': 'accesorio'})
 
-format_error = ['unit_sale_price (CLP)', 'cost (CLP)', 'storage_cost (CLP)', 'stock', 'cost_per_purchase']
-items[format_error] = items[format_error].astype(int)
-
 ########################### DATA_PURCHASES ###########################
-
-'''
-Cosas por arreglar:
-    (1) Cantidades == 0
-    (2) NO siempre hay correspondencia entre el costo descrito en esta tabla y data_items
-    (3) Formato de atributos
-    (4) Fechas
-'''
 
 data_purchases1 = pd.read_csv('data_purchases.csv', delimiter=';')
 data_purchases2 = pd.read_csv('data_purchases_2.csv', delimiter=',')
 data_purchases = pd.concat([data_purchases1, data_purchases2], ignore_index=True)
 
-purchases = data_purchases[~data_purchases['item_id'].isin(descartados)].copy()
-format_error = ['cost (CLP)']
-purchases[format_error] = purchases[format_error].astype(int)
-
-purchases['date'] = pd.to_datetime(purchases['date'], format='%Y-%m-%d')
-purchases['delivery_date'] = pd.to_datetime(purchases['delivery_date'], format='%Y-%m-%d')
-
-purchases['delivery_time (days)'] = (purchases['delivery_date'] - purchases['date']).dt.days
+purchases = data_purchases.copy()
 
 ########################### DATA_SALES ###########################
 
-'''
-Cosas por arreglar:
-    (1) Formato de atributos
-    (2) Fechas
-'''
 
 data_sales = pd.read_csv('data_sales.csv', delimiter=';')
+#print(data_sales[data_sales['item_id']==1790])
 
-sales = data_sales[~data_sales['item_id'].isin(descartados)].copy()
-
-format_error = ['unit_sale_price (CLP)', 'total (CLP)', 'client_id']
-sales[format_error] = sales[format_error].astype(int)
-
-sales['date'] = pd.to_datetime(sales['date'], format='%Y-%m-%d')
+#sales = data_sales[~data_sales['item_id'].isin(descartados)].copy()
+sales = data_sales.copy()
 
 #items['Suma'] = items['size_m3'] * items['stock'] #Para ver el stock del archivo items
 #print(items['Suma'].sum())
 
+######################## Se sacan los productos descontinuados ########################
+
 sales['date'] = pd.to_datetime(sales['date']) # productos descontinuados
-cutoff_date = pd.Timestamp('2022-10-04')
+cutoff_date = pd.Timestamp('2022-10-04') # son descontinuados aquellos que no se venden hace casi 14 meses
 last_sale_dates = sales.groupby('item_id')['date'].max()
 discontinued_items = last_sale_dates[last_sale_dates < cutoff_date]
 discontinued_item_ids = discontinued_items.index
@@ -87,91 +57,82 @@ items = items[items['descontinuado'] == 0]
 sales = sales[sales['item_id'].isin(items['item_id'])]
 purchases = purchases[purchases['item_id'].isin(items['item_id'])]
 
-
 #print(f"% datos perdidos:{1 - items['item_id'].count()/data_items['item_id'].count()}") # 0.17647058823529416
 #print(f"% datos perdidos:{1 - purchases['item_id'].count()/data_purchases['item_id'].count()}") # 0.36868986424146566
 #print(f"% datos perdidos:{1 - sales['item_id'].count()/data_sales['item_id'].count()}") # 0.7204518863551072
 
-
-#############################################################################
-#                  CHECHO, EMPIEZA A TRABAJAR DESDE AQUÍ                    #
-#############################################################################
-
+####################### Se recupera la inf de costo y precio de venta!!!! ####################### en promedio hay 23 valores por col que no se pudieron recuperar
 '''
-    (1) clasificar la demanda, en el dataframe de 'items' crear columnas con los nombres: 
-        [frecuencia, estacionalidad, tendencia, intermitente]
-        La idea es rellenar con numeros 1, 0 o -1 (de ser el caso), 
-        los criterios para cada caracteristica los defines tú (trata de tenerlos anotados)
-
-    (2) Tratar los productos que tienen stock inicial negativo
-
-    (3)
-
-    (4) Solo en caso de tener tiempo (cuando termines el resto de arreglos de los archivos, 
-        enfocate en esto), buscar la forma de reducir el nº de observaciones perdidas en todas las tablas
-        (items, sales y purchases) sobre todo en las dos últimas
+print(items.head())
+print(items.columns.values)
+print(sales.head())
+print(purchases.head())
 '''
+purchases_no_duplicates = purchases.groupby('item_id').agg({
+    'cost (CLP)': 'mean'  # O puedes usar 'max', 'min' según el caso
+}).reset_index()
 
-sales_per_item = sales.groupby('item_id')['date'].count()
+columns_to_replace = ['cost (CLP)']
+for col in columns_to_replace:
+    items[col] = items[col].fillna(items['item_id'].map(purchases_no_duplicates.set_index('item_id')[col]))
 
-total_periods = sales['date'].nunique()  # Total de períodos únicos (días/meses)
+sales_no_duplicates = sales.groupby('item_id').agg({
+    'unit_sale_price (CLP)': 'mean' # O puedes usar 'max', 'min' según el caso
+}).reset_index()
 
-alta_frecuencia_threshold = 0.7 * total_periods # por qué 30% y 70%
-baja_frecuencia_threshold = 0.3 * total_periods
+columns_to_replace = ['unit_sale_price (CLP)']
+for col in columns_to_replace:
+    # Usamos .map() para obtener los valores de purchases que coinciden con item_id
+    items[col] = items[col].fillna(items['item_id'].map(sales_no_duplicates.set_index('item_id')[col]))
 
-items['frecuencia'] = sales_per_item.apply(
-    lambda x: 1 if x >= alta_frecuencia_threshold else (-1 if x <= baja_frecuencia_threshold else 0))
+####################### Falta recuperar inf. sobre storage_cost, size:m3 y cost_per_purchase ####################### 
 
-# Paso 1: Buscar productos con estacionalidad -----> mensual???? REVISAR NO ES CONFIABLE
-sales['month'] = pd.to_datetime(sales['date']).dt.month
-monthly_sales = sales.groupby(['item_id', 'month'])['unit_sale_price (CLP)'].sum().unstack(fill_value=0)
+columns_to_fill = ['storage_cost (CLP)', 'size_m3', 'cost_per_purchase']
 
-items['estacionalidad'] = monthly_sales.apply(lambda row: 1 if row.max() >= 2 * row.mean() else 0, axis=1)
-#print(items[items['estacionalidad']==1])
-# Paso 2: Tratar productos con stock inicial negativo
-# Creamos la columna 'tendencia' basada en los datos de stock inicial
-items['tendencia'] = items['stock'].apply(lambda x: 1 if x > 0 else 0)
+for col in columns_to_fill:
+    # Calcular el promedio de cada columna por categoría
+    category_means = items.groupby('group_description')[col].transform('mean')
+    
+    # Reemplazar valores NaN con el promedio de la categoría correspondiente
+    items[col] = items[col].fillna(category_means)
 
-# Paso 3: Definir si es intermitente
-# Un producto es intermitente si tiene más de un 50% de períodos sin ventas, pero en los días que tiene ventas, estas son significativas
-zero_sales_per_item = total_periods - sales_per_item  # Número de períodos sin ventas
-items['intermitente'] = zero_sales_per_item.apply(lambda x: 1 if x > 0.5 * total_periods else 0)
+#print(items.isna().sum()) # hay 147 productos sin categoría, se podría poner a mano pero es mucho trabajo... se dejará por minetras como el promedio de toda la tienda para todas las columnas
 
-# Mostrar el DataFrame con las nuevas columnas
-#print(items[['frecuencia', 'estacionalidad', 'tendencia', 'intermitente']])
+columns_to_fill = ['storage_cost (CLP)', 'size_m3', 'cost_per_purchase', 'unit_sale_price (CLP)', 'cost (CLP)']
+overall_means = items[columns_to_fill].mean()
+for col in columns_to_fill:
+    # Filtrar los productos sin categoría (group_description == NaN)
+    items.loc[items['group_description'].isna(), col] = items.loc[items['group_description'].isna(), col].fillna(overall_means[col])
 
-# Guardar el nuevo DataFrame con las columnas adicionales
-items.to_csv('items_with_forecasting_features.csv', index=False)
+items['stock'] = items['stock'].fillna(0) # se supondrá que no hay productos..... REVISAR
 
-# Extraer los primeros 5 productos de cada tipo de demanda basados en las columnas que hemos creado
-alta_frecuencia = items[items['frecuencia'] == 1].head(5)
-baja_frecuencia = items[items['frecuencia'] == -1].head(5)
-continua = items[items['frecuencia'] == 0].head(5)
-intermitente = items[items['intermitente'] == 1].head(5)
-estacional = items[items['estacionalidad'] == 1].head(5)
+#print(items.isna().sum())
 
-# Guardar los nombres de los primeros 5 productos de cada tipo
-tipos_de_demanda = {
-    'Alta Frecuencia': alta_frecuencia['item_id'].tolist(),
-    'Baja Frecuencia': baja_frecuencia['item_id'].tolist(),
-    'Continua': continua['item_id'].tolist(),
-    'Intermitente': intermitente['item_id'].tolist(),
-    'Estacional': estacional['item_id'].tolist()
-}
 
-#print(tipos_de_demanda)
+############################### AHORA SE CAMBIAN LOS FORMATOS ###############################
 
-baja_frecuencia_items = items[items['item_id'].isin([1701, 275, 416, 1655, 247])]
-continua_items = items[items['item_id'].isin([203, 1592, 208, 973, 845])]
-intermitente_items = items[items['item_id'].isin([1701, 275, 416, 1655, 247])]
-estacional_items = items[items['item_id'].isin([1701, 275, 416, 247, 383])]
+format_error = ['unit_sale_price (CLP)', 'cost (CLP)', 'storage_cost (CLP)', 'stock', 'cost_per_purchase']
+items[format_error] = items[format_error].astype(int)
 
-productos_encontrados = {
-    'Baja Frecuencia': baja_frecuencia_items[['item_id', 'description']].to_dict(orient='records'),
-    'Continua': continua_items[['item_id', 'description']].to_dict(orient='records'),
-    'Intermitente': intermitente_items[['item_id', 'description']].to_dict(orient='records'),
-    'Estacional': estacional_items[['item_id', 'description']].to_dict(orient='records')
-}
+format_error = ['cost (CLP)']
+purchases[format_error] = purchases[format_error].astype(int)
+# purchases['date'] = pd.to_datetime(purchases['date'], infer_datetime_format=True)
+# purchases['delivery_date'] = pd.to_datetime(purchases['delivery_date'], infer_datetime_format=True)
+# purchases['delivery_time (days)'] = (purchases['delivery_date'] - purchases['date']).dt.days
 
-#print(productos_encontrados)
+format_error = ['unit_sale_price (CLP)', 'total (CLP)']
+sales[format_error] = sales[format_error].astype(int)
+sales['date'] = pd.to_datetime(sales['date'], format='%Y-%m-%d')
 
+############################### Añadir la columna 'lead_time_promedio' en la tabla 'items' ###############################
+def calcular_lead_time_promedio(items, purchases):
+    lead_time_promedio = purchases.groupby('item_id')['delivery_time (days)'].mean().reset_index()
+    lead_time_promedio['delivery_time (days)'] = lead_time_promedio['delivery_time (days)'].fillna(0)
+    lead_time_promedio = lead_time_promedio.rename(columns={'delivery_time (days)': 'lead_time_promedio'})
+    items = items.merge(lead_time_promedio, on='item_id', how='left')
+    items['lead_time_promedio'] = items['lead_time_promedio'].fillna(0)
+    return items
+
+# items = calcular_lead_time_promedio(items, purchases) # es un float
+
+#print(purchases[purchases['quantity']==0]) ######### muchas compras por 0 productos????? por el momento no es relevante.
